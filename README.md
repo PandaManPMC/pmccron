66a73708e7a7b9b142081de12e745403d4d0fe8a
PandaManPMC <weiqudedawei@outlook.com>
2022-08-27 16:32:26 +0800

    使用方式

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # pmccron
 定时任务调度器
+
+#### 使用方式
+
+```go
+func main() {
+	scheduler := pmccron.InitSchedulerSingle(func(msg string) {
+		fmt.Println(msg)
+	}, func(msg string, err interface{}) {
+		fmt.Printf("%s----------%v", msg, err)
+	})
+	scheduler.DayHour("08", func() {
+		fmt.Println("执行每天 08 点定时任务")
+	})
+	scheduler.DayHour("16", func() {
+		fmt.Println("执行每天 16 点定时任务")
+	})
+	scheduler.Minute("15", func() {
+		fmt.Println("执行每时的第 15 分的定时任务")
+	})
+
+	scheduler.Running()
+	time.Sleep(24 * time.Hour)
+}
+```
+
+
--- example/schedulerTaskExample.go
+++ example/schedulerTaskExample.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"pmccron/pmccron"
-	"sync"
 	"time"
 )
 
@@ -12,10 +11,6 @@ import (
 //  desc: example
 
 func main() {
-
-	wg := sync.WaitGroup{}
-	wg.Add(2)
-
 	scheduler := pmccron.InitSchedulerSingle(func(msg string) {
 		fmt.Println(msg)
 	}, func(msg string, err interface{}) {
@@ -23,17 +18,14 @@ func main() {
 	})
 	scheduler.DayHour("08", func() {
 		fmt.Println("执行每天 08 点定时任务")
-		wg.Done()
 	})
 	scheduler.DayHour("16", func() {
 		fmt.Println("执行每天 16 点定时任务")
-		wg.Done()
 	})
 	scheduler.Minute("15", func() {
-		fmt.Println("执行每 15 分钟执行一次的定时任务")
+		fmt.Println("执行每时的第 15 分的定时任务")
 	})
 
 	scheduler.Running()
-	//wg.Wait()
 	time.Sleep(24 * time.Hour)
 }
